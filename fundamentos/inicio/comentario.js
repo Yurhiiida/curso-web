fb798ca50bf02885eac1c0881695e5cf3cd0055e
Yurhi Iida <99679115+Yurhiiida@users.noreply.github.com>
2022-04-24 11:46:48 -0300

    fundamentos

---
+++ fundamentos/inicio/comentario.js
@@ -0,0 +1,14 @@
+// console.log é usado para exibir algo na tela do computador!
+// barra barra é usado pra fazer comentario em linha.
+
+/*
+console.log('Passo #01');
+console.log('Passo #01');
+console.log('Passo #01'); */
+
+/*
+ * Barra asteristico
+ * serve para 
+ * comentario em
+ * múltiplas linhas 
+ */
