5cad22163553bd73d3ded4cec5c18b263160aa4e
Yurhi Iida <99679115+Yurhiiida@users.noreply.github.com>
2022-04-25 17:47:59 -0300

    continuação dos fundamentos de js

---
+++ exercicios-js/fundamentos/hoisting.js
@@ -0,0 +1,16 @@
+console.log('a =', a)
+var a = 2
+console.log('a =', a)
+
+// hoisting
+// var a
+// console.log('a =', a)
+// a = 2
+// console.log('a =', a)
+
+//mesma coisa acontece dentro de uma function
+
+//Agora usando let não acontece o hoisting, e da erro no codigo
+console.log('b =', b)
+let b = 2
+console.log('b =', b)
---
+++ exercicios-js/fundamentos/objetos.js
@@ -0,0 +1,14 @@
+const prod1 = {} //objeto
+prod1.nome = 'Celular Ultra Mega' //formas de adicionar elementos ao objeto
+prod1.preco = 4998.90
+prod1['Desconto Legal'] = 0.40
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90,
+} //outra forma de criar um objeto.
+
+
+console.log(prod2)
