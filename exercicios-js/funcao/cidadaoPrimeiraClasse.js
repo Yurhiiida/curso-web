6fc02baaed991d4f8fca7fc959c312778f516598
Yurhi Iida <99679115+Yurhiiida@users.noreply.github.com>
2022-04-29 22:40:27 -0300

    exercicios js

---
+++ exercicios-js/funcao/cidadaoPrimeiraClasse.js
@@ -0,0 +1,37 @@
+//Função em JS é First-Class Object (Citizens)
+//Higher-order function
+
+//Criar de forma literal
+function func1(a, b) {
+    return a + b
+}
+
+//Armazenar em uma variável
+const func2 = function(a, b){
+    return a * b
+}
+
+//Armazenar em um array
+const array = [function (a,b) {return a + b}, func1, func2]
+console.log(array[0](2, 3), array[1](10, 5), array[2](25, 2))
+
+//Armazenar em um atributo de objeto
+const obj = {}
+obj.falar = function() {return 'Opa'}
+console.log(obj.falar())
+
+//Passar função como parametro
+function run(fun) {
+    fun()
+}
+
+run(function() { console.log('Executando...')})
+
+//Uma função pode retornar/conter uma função
+function soma(a,b) {
+    return function(c) {
+        console.log(a + b + c)
+    }
+}
+
+soma(2, 3)(4)
